5cb2c498fe5e8b98fb22c98ba924b6e215ccbd6b
LeNerd46 <devon.sparkle2012@gmail.com>
2022-03-02 20:19:27 -0600

    Somewhat fixed dropdown being expanded on small screens
    Before, the dropdown would take up space even when it wasn't expanded. I also did some nice spacing with the logo and the team name

--- script.js
+++ script.js
@@ -3,25 +3,32 @@ const $dropdownToggle = $(".dropdown-toggle");
 const $dropdownMenu = $(".dropdown-menu");
 const showClass = "show";
 
+// Only make it work on not very small screens???
+// if (this.matchMedia("(min-width: 768px)").matches)
+
 $(window).on("load resize", function() {
-  if (this.matchMedia("(min-width: 768px)").matches) {
     $dropdown.hover(
       function() {
         const $this = $(this);
+        
         $this.addClass(showClass);
         $this.find($dropdownToggle).attr("aria-expanded", "true");
         $this.find($dropdownMenu).addClass(showClass);
+        $dropdownMenu.removeClass('d-none');
+        
         $("#navbarDropdown").addClass('special');
       },
       function() {
         const $this = $(this);
+        
         $this.removeClass(showClass);
         $this.find($dropdownToggle).attr("aria-expanded", "false");
         $this.find($dropdownMenu).removeClass(showClass);
+        $dropdownMenu.addClass('d-none');
+        
         $("#navbarDropdown").removeClass('special');
       }
+      
     );
-  } else {
-    $dropdown.off("mouseenter mouseleave");
-  }
+  
 });
